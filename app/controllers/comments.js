304c0627b1b43c0c8a4554379a4f10e72d6c982b
Henry Baughman <hbaughman741@gmail.com>
2015-06-06 15:39:32 -0400

    Configure snoocore headers

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
   init: function() {
-    this.set('reddit', new window.Snoocore({
-      userAgent: '/u/embeddit-reddit embeddit-reddit@0.0.1',
+
+    var reddit = new window.Snoocore({
+      userAgent: '/u/embeddit-reddit embeddit-reddit@0.0.1',   
       oauth: {
         type: 'implicit',
-        key: '7LY_7ViAfIQNkw',
+        key: '7LY_7ViAfIQNkw',   
         redirectUri: 'http://localhost:8000/',
-        scope: [ 'identify', 'read' ]
+        scope: [ 'identity', 'read' ]
       }
-    }));
-  }
+    });
+    this.set('reddit', reddit);
+  },
 });
