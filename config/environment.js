a5a8629582d30f83b445db596b158b71c22c93c0
Henry Baughman <hbaughman741@gmail.com>
2015-06-06 17:51:12 -0400

    Child comments now extend to right margin

--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,8 @@ module.exports = function(environment) {
       // when it is created
     },
     contentSecurityPolicy: {
-      'connect-src': '*'
+      'connect-src': '*',
+      'image-src': "'self', http://www.redditstatic.com"
     }
   };
 
